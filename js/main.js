fbd26e86f0559d94815091ac5daf60436d5655a3
albertogioia93 <albertogioia@hotmail.it>
2023-02-21 16:04:43 +0100

    errore js riga 15

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// chiedo all’utente il numero di chilometri che vuole percorrere
+
+const kmPreferiti = parseInt(prompt('Quanti chilometri vuoi percorrere?'));
+console.log(kmPreferiti);
+
+// chiedo all'utente la sua età
+
+const etaUtente = parseInt(prompt('Inserisci la tua età'));
+console.log(etaUtente);
+
+// creo variabile che è risultato tra kmPreferiti e la tariffa di 0,21€ al km
+
+let prezzoBiglietto = kmPreferiti * 0,21;
+console.log(prezzoBiglietto);
